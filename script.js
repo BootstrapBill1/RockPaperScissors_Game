93efaa87a1acbc6951980fd9b081fb1e02b42754
Tanner Lynn <tanner.lynn39@gmail.com>
2022-10-01 17:34:38 -0600

    moidfy the UI and rewrite the game logic to fit the user

--- script.js
+++ script.js
@@ -1,3 +1,42 @@
+const buttons = document.querySelectorAll("div.buttons > button");
+const container = document.querySelector("body");
+const score = document.createElement("div");
+score.style.display = "flex";
+score.style.justifyContent = "space-around";
+score.style.fontSize = "20px";
+score.style.backgroundColor = "green";
+score.style.fontWeight ="Bold";
+score.style.color = "white";
+score.classList.add("scores");
+const user = document.createElement("div");
+const computer = document.createElement("div");
+user.classList.add("user");
+computer.classList.add("computer");
+user.textContent = "User: 0";
+computer.textContent = "Computer: 0";
+score.appendChild(user);
+score.appendChild(computer);
+container.appendChild(score);
+
+
+let timesPlayed = 0;
+
+buttons.forEach((button) => {
+
+    button.addEventListener('click', () =>{
+        play(button.textContent);
+    });
+});
+
+function play(string){
+    game(string, ++timesPlayed);
+
+
+}
+
+
+
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) +1;
     if(randomNumber == 1){
@@ -13,12 +52,12 @@ function getComputerChoice() {
 
 }
 
-function playRound(){
+function playRound(playerSelection){
     let computerSelection = getComputerChoice();
     console.log("Computer: " + computerSelection);
-    let playerSelection = prompt("Enter either Rock, Paper, Scissors:");
+    //let playerSelection = prompt("Enter either Rock, Paper, Scissors:");
     playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-    console.log(playerSelection);
+    console.log("User: " + playerSelection);
     if(playerSelection == "Rock"){
         if(computerSelection == "Rock"){
             return "Its a tie";
@@ -58,19 +97,29 @@ function playRound(){
 
 }
 
-function game(){
-    let userScore = 0;
-    let computerScore = 0;
-    for(let i =0; i < 5; i++){
-        score = playRound()
-        console.log(score);
-        if (score.slice(0,7) == "You Win"){
-            userScore++;
-        }
-        else if (score.slice(0,8) == "You Lose"){
-            computerScore++;
-        }
+let userScore = 0;
+let computerScore = 0;
+
+function game(playerSelection, timesPlayed){
+
+    playerScore = playRound(playerSelection)
+    console.log(playerScore);
+    if (playerScore.slice(0,7) == "You Win"){
+        userScore++;
+        user.textContent = "User: " + userScore;
+
     }
+    else if (playerScore.slice(0,8) == "You Lose"){
+        computerScore++;
+        computer.textContent = "Computer: " + computerScore;
+    }
+
+    if(timesPlayed == 5){
+        endGame(userScore, computerScore);
+    }
+}
+
+function endGame(userScore, computerScore){
     if(userScore > computerScore){
         console.log("You win the game");
     }
@@ -79,5 +128,3 @@ function game(){
     }
     console.log("You: " + userScore + " Computer: " + computerScore);
 }
-
-game();
